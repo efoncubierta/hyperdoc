8f7c5219221cc0c20593708cff7bb0a19cde03f4
Ezequiel Foncubierta <ezequiel.foncubierta@gmail.com>
2018-06-07 00:30:31 +0200

    Added AWS node store tests

--- test/store/aws/index.ts
+++ test/store/aws/index.ts
@@ -2,10 +2,12 @@ import "mocha";
 
 // test suites
 import mappingDynamoDBStoreTests from "./MappingDynamoDBStore.test";
+import nodeDynamoDBStoreTests from "./NodeDynamoDBStore.test";
 
 function storeTests() {
   describe("AWS", () => {
     mappingDynamoDBStoreTests();
+    nodeDynamoDBStoreTests();
   });
 }
 
